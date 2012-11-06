cef4ffaee66d40dfe4b586d8f936e7945fab56d6
matt40k_cp <SND\matt40k_cp>
2012-11-06 15:36:30 -0800

    Check-in into Dev - 0.1

---
+++ Dev/0.1/dbSend.Cmd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dbSend.Process;
+
+namespace dbSend.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Invoke dbSend class
+            Entry entry = new Entry();
+
+            string testFile = @"C:\Windows\system32\drivers\etc\hosts";
+
+            Console.WriteLine(entry.SetUploadFile(testFile));
+            Console.WriteLine(entry.GetRandomPassword);
+            Console.WriteLine(entry.DoIt);
+            Console.ReadKey();
+        }
+    }
+}
